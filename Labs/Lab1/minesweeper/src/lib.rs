aac0d1f8ae9b9bd753f3fd751ee595e13a40609c
Alessio Rosiello <alex.robassomero@gmail.com>
2023-03-22 15:22:12 +0100

    Esercizio 3

---
+++ Labs/Lab1/minesweeper/src/lib.rs
@@ -0,0 +1,91 @@
+fn count_adjacent(i: i64, j: i64, matrix: &[&[u8]]) -> u8 {
+    let row_count = (matrix.len() - 1) as i64;
+    let col_count = (matrix[i as usize].len() - 1) as i64;
+    let indexes = vec![
+        (1, 1),
+        (1, -1),
+        (-1, 1),
+        (-1, -1),
+        (0, 1),
+        (0, -1),
+        (1, 0),
+        (-1, 0),
+    ];
+    let mut count = 0;
+    for (x_off, y_off) in indexes {
+        let x = i + x_off;
+        let y = j + y_off;
+        if x >= 0
+            && x <= row_count
+            && y >= 0
+            && y <= col_count
+            && matrix[x as usize][y as usize] == b'*'
+        {
+            count += 1;
+        }
+    }
+    count
+}
+
+pub fn annotate2(minefield: String, rows: usize, cols: usize) -> String {
+    let indexes: Vec<(i64, i64)> = vec![
+        (1, 1),
+        (1, -1),
+        (-1, 1),
+        (-1, -1),
+        (0, 1),
+        (0, -1),
+        (1, 0),
+        (-1, 0),
+    ];
+    let mut bytes = minefield.into_bytes();
+    for i in 0..rows {
+        for j in 0..cols {
+            let cell = bytes[i * cols + j];
+            if cell == b'*' {
+                continue;
+            }
+            let mut count = 0;
+            for (x_off, y_off) in indexes.iter() {
+                let x = i as i64 + x_off;
+                let y = j as i64 + y_off;
+                if x >= 0
+                    && x < rows as i64
+                    && y >= 0
+                    && y < cols as i64
+                    && bytes[(x * cols as i64 + y) as usize] == b'*'
+                {
+                    count += 1;
+                }
+            }
+            if count == 0 {
+                continue;
+            }
+            bytes[i * cols + j] = b'0' + count;
+        }
+    }
+    String::from_utf8(bytes).unwrap()
+}
+
+pub fn annotate(minefield: &[&str]) -> Vec<String> {
+    let mut result = Vec::new();
+    let bytes_minefield = minefield.iter().map(|s| s.as_bytes()).collect::<Vec<_>>();
+    for (i, row) in bytes_minefield.iter().enumerate() {
+        let mut current_row = String::new();
+        for (j, cell) in row.iter().enumerate() {
+            if *cell == b'*' {
+                current_row.push('*');
+            } else {
+                let count = count_adjacent(i as i64, j as i64, &bytes_minefield);
+                if count == 0 {
+                    current_row.push(' ');
+                }
+                else {
+                    current_row.push((b'0' + count) as char);
+                }
+            }
+        }
+        result.push(current_row);
+    }
+    result
+}
---
+++ Labs/Lab1/minesweeper/src/main.rs
@@ -0,0 +1,39 @@
+use clap::Parser;
+use minesweeper::{annotate, annotate2};
+
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    rows: usize,
+    #[arg(short, long)]
+    cols: usize,
+    board: String
+}
+
+fn transform_board(rows: usize, cols: usize, input_board: &str) -> Vec<&str> {
+    let mut board = vec![];
+    for i in 0..rows {
+        let start_idx = i * cols;
+        let end_idx = start_idx + cols;
+        let row = &input_board[start_idx..end_idx];
+        board.push(row);
+    }
+    board
+}
+
+fn main() {
+    let args = Args::parse();
+    let board = transform_board(args.rows, args.cols, &args.board);
+    let result = annotate(&board);
+    let result_single_string = annotate2(args.board, args.rows, args.cols);
+    let transformed_result = transform_board(args.rows, args.cols, &result_single_string);
+    println!("***Result with annotate2: ***");
+    for row in transformed_result {
+        println!("{}", row);
+    }
+    println!("***Result with annotate : ***");
+    for row in result {
+        println!("{}", row);
+    }
+}
