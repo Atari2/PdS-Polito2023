9b2e0c9bd03e0d83d49c5a061e972e84a92a963c
Alessio Rosiello <alex.robassomero@gmail.com>
2023-03-22 13:37:57 +0100

    Add tests.

--- Labs/Lab1/capitalize/src/main.rs
+++ Labs/Lab1/capitalize/src/main.rs
@@ -1,6 +1,6 @@
 use clap::Parser;
 /*
-La funzione deve convertire in maiuscolo il primo carattere di ogni parola che compone il 
+La funzione deve convertire in maiuscolo il primo carattere di ogni parola che compone il
 testo s, ignorando eventuali altri caratteri maiuscoli al suo interno.
 Le parole sono separate da uno o più spazi
 */
@@ -23,6 +23,42 @@ fn capitalize(s: &str) -> String {
 #[derive(Parser, Debug)]
 struct Args {
     input: String,
+}
+
+/*
+ stringa con più di una parola
+ stringa con una sola parola senza spazi
+ stringa con caratteri accentati all’inizio di parola
+ stringa vuota
+ stringa con più spazi
+*/
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_multiple_words() {
+        assert_eq!(capitalize("ciao mondo"), "Ciao Mondo");
+    }
+    #[test]
+    fn test_one_word() {
+        assert_eq!(capitalize("ciao"), "Ciao");
+    }
+    #[test]
+    fn test_with_accents() {
+        assert_eq!(
+            capitalize("il sole è sorto, àèìòù"),
+            "Il Sole È Sorto, Àèìòù"
+        );
+    }
+    #[test]
+    fn test_empty_string() {
+        assert_eq!(capitalize(""), "");
+    }
+    #[test]
+    fn test_multiple_spaces() {
+        assert_eq!(capitalize("ciao   mondo"), "Ciao   Mondo");
+    }
 }
 
 fn main() {
